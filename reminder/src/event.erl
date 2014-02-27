72f473e418f392b3601776167da3bffa4972f34f
Jon Grimes <jonkgrimes@gmail.com>
2014-02-27 07:57:52 -0600

    Some interesting process management stuff

--- reminder/src/event.erl
+++ reminder/src/event.erl
@@ -10,14 +10,24 @@ loop(S = #state{server=Server,to_go=[T|Next]}) ->
       Server ! { Ref, ok }
   after T*1000 -> 
     if Next =:= [] ->
-      Server ! { done, S#state.name };
-    Next =/= [] ->
-      loop(S#state{to_go=Next})
+        Server ! { done, S#state.name };
+      Next =/= [] ->
+        loop(S#state{to_go=Next})
+    end
   end.
 
 normalize(N) ->
   Limit = 49 * 24 * 60 * 60,
   [N rem Limit | lists:duplicate(N div Limit, Limit)].
+
+time_to_go(TimeOut={{_,_,_}, {_,_,_}}) ->
+  Now = calendar:local_time(),
+  ToGo = calendar:datetime_to_gregorian_seconds(TimeOut) -
+    calendar:datetime_to_gregorian_seconds(Now),
+  Secs = if ToGo > 0 -> ToGo,
+      ToGo =< 0 -> 0
+  end,
+  normalize(Secs).
 
 start(EventName, Delay) ->
   spawn(?MODULE, init, [self(), EventName, Delay]).
@@ -26,14 +36,14 @@ start_link(EventName, Delay) ->
   spawn_link(?MODULE, init, [self(), EventName, Delay]).
 
 %% Events innards
-init(Server, EventName, Delay) ->
+init(Server, EventName, DateTime) ->
   loop(#state{server=Server,
       name=EventName,
-      to_go=normalize(Delay)}).
+      to_go=time_to_go(DateTime)}).
 
 cancel(Pid) ->
   %% Monitor in case the process is already dead
-  Ref = elrang:monitor(process,Pid),
+  Ref = erlang:monitor(process,Pid),
   Pid ! { self(), Ref, cancel },
   receive
     { Ref, ok } ->
