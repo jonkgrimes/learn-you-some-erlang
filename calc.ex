8af6f6ed3421a4a2a53b9bbe6a5eb88206fda636
Jon Grimes <jonkgrimes@gmail.com>
2014-03-10 08:50:04 -0500

    There are some slight differences, but looks pretty similar

--- calc.ex
+++ calc.ex
@@ -1,11 +1,55 @@
 defmodule Calc do
-  def rpn(list) when is_string(list) do
-    IO.puts "Hello"
+  def rpn(list) do
     [response] = List.foldl(String.split(list," "),[],&rpn(&1,&2))
     response
   end
 
   def rpn("+",[n1,n2|s]) do
     [n2 + n1 | s]
+  end
+
+  def rpn("sum",[n1,n2,n3,n4|s]) do
+    [n4+n3+n2+n1|s]
+  end
+
+  def rpn("-",[n1,n2|s]) do
+    [n2 - n1 | s]
+  end
+
+  def rpn("*",[n1,n2|s]) do
+    [n2 * n1 | s]
+  end
+
+  def rpn("prod",[n1,n2,n3,n4|s]) do
+    [n4*n3*n2*n1|s]
+  end
+
+  def rpn("/",[n1,n2|s]) do
+    [n2/n1|s]
+  end
+
+  def rpn("^",[n1,n2|s]) do
+    [:math.pow(n2,n1)|s]
+  end
+
+  def rpn("ln",[n|s]) do
+    [:math.log(n)|s]
+  end
+
+  def rpn("log10",[n|s]) do
+    [:math.log10(n)|s]
+  end
+
+  def rpn(x,stack) do
+    [read(x)|stack]
+  end
+
+  def read(n) do
+    case Float.parse(n) do
+      :error ->
+        list_to_integer(n)
+      { f, _ } ->
+        f
+    end
   end
 end
--- calc_test.exs
+++ calc_test.exs
@@ -6,4 +6,40 @@ defmodule CalcTest do
   test "addition" do
     assert Calc.rpn("2 3 +") == 5
   end
+
+  test "subtraction" do
+    assert Calc.rpn("3 2 -") == 1
+  end
+
+  test "multiplication" do
+    assert Calc.rpn("2 3 *") == 6
+  end
+
+  test "division" do
+    assert Calc.rpn("10 5 /") == 2
+  end
+
+  test "exponent" do
+    assert Calc.rpn("5 2 ^") == 25
+  end
+
+  test "natural log" do
+    assert Calc.rpn("2.7 ln") == :math.log(2.7)
+  end
+
+  test "log" do
+    assert Calc.rpn("2.7 log10") == :math.log10(2.7)
+  end
+
+  test "multiple terms" do
+    assert Calc.rpn("10 4 3 + 2 * -") == -4
+    assert Calc.rpn("10 4 3 + 2 * - 2 /") == -2.0
+    assert Calc.rpn("90 34 12 33 55 66 + * - + -") == 4037
+  end
+
+  test"raises badmatch" do
+    assert_raise MatchError, fn ->
+      Calc.rpn("90 34 12 33 55 66 + * - +")
+    end
+  end
 end
