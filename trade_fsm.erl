1f693190ac4b15ad4dc40d2932018016563ceeee
Jon Grimes <jonkgrimes@gmail.com>
2014-03-16 16:44:33 -0500

    Add a couple more things

--- trade_fsm.erl
+++ trade_fsm.erl
@@ -30,13 +30,22 @@ unexpected(Msg, State) ->
     [self(),Msg,State]).
 
 %% idle
-idle({ ask_negotiate, OtherPid }, S#state{}) ->
+idle({ ask_negotiate, OtherPid }, S=#state{}) ->
   Ref = monitor(process, OtherPid),
   notice(S, "~p asked for trade a negotiation", [OtherPid]),
   { next_state, idle_wait, S#state{other=OtherPid,monitor=Ref}};
-idle(Event,Date) ->
+idle(Event,Data) ->
   unexpected(Event, idle),
   { next_state, idle, Data}.
+
+idle({negotiate, OtherPid}, From, S=#state{}) ->
+  ask_negotiate(OtherPid, self()),
+  notice(S, "asking user ~p for a trade", [OtherPid]),
+  Ref = monitor(process, OtherPid),
+  {next_state, idle_wait, S#state{other=OtherPid, monitor=Ref, from=From}};
+idle(Event, _From, Data) ->
+  unexpected(Event, idle),
+  {next_state, idle, Data}.
 
 %% idle_wait
 idle_wait({ ask_negotiate, OtherPid }, S=#state{other=OtherPid}) ->
@@ -59,6 +68,147 @@ idle_wait(Event, _From, Data) ->
   unexpected(Event, idle_wait),
   {next_state, idle_wait, Data}.
 
+%% negotiate
+negotiate({ make_offer, Item }, S=#state{ownitems=OwnItems}) ->
+  do_offer(S#state.other, Item),
+  notice(S, "offering ~p", [Item]),
+  { next_state, negotiate, S#state{ownitems=add(Item,OwnItems)}};
+negotiate({ retract_offer, Item }, S=#state{ownitems=OwnItems}) ->
+  undo_offer(S#state.other, Item),
+  notice(S, "cancelling offer on ~p", [Item]),
+  { next_state, negotiate, S#state{ownitems=remove(Item,OwnItems)}};
+negotiate({ do_offer, Item}, S=#state{otheritems=OtherItems}) ->
+  notice(S, "other player offering ~p", [Item]),
+  { next_state, negotiate, S#state{otheritems=add(Item,OtherItems)}};
+negotiate({ undo_offer, Item}, S=#state{otheritems=OtherItems}) ->
+  notice(S, "other player cancelling offer on ~p", [Item]),
+  { next_sate, negotiate, S#state{otheritems=remove(Item,OtherItems)}};
+negotiate(are_you_ready, S=#state{other=OtherPid}) ->
+  io:format("Other user ready to trade.~n"),
+  notice(S,
+    "Other user ready to transfer goods:~n"
+    "You get ~p, the other side gets ~p",
+    [S#state.otheritems,S#state.ownitems]),
+  not_yet(OtherPid),
+  { next_state, negotiate, S };
+negotiate(Event, Data) ->
+  unexpected(Event, negotiate),
+  { next_state, negotiate, Data }.
+
+negotiate(ready, From, S = #state{other=OtherPid}) ->
+  are_you_ready(OtherPid),
+  notice(S, "asking if ready, waiting", []),
+  {next_state, wait, S#state{from=From}};
+negotiate(Event, _From, S) ->
+  unexpected(Event, negotiate),
+  {next_state, negotiate, S}.
+
+%% wait
+wait({do_offer, Item}, S=#state{otheritems=OtherItems}) ->
+  gen_fsm:reply(S#state.from, offer_changed),
+  notice(S, "other side offering ~p", [Item]),
+  {next_state, negotiate, S#state{otheritems=add(Item, OtherItems)}};
+wait({undo_offer, Item}, S=#state{otheritems=OtherItems}) ->
+  gen_fsm:reply(S#state.from, offer_changed),
+  notice(S, "Other side cancelling offer of ~p", [Item]),
+  {next_state, negotiate, S#state{otheritems=remove(Item, OtherItems)}};
+wait(are_you_ready, S=#state{}) ->
+  am_ready(S#state.other),
+  notice(S, "asked if ready, and I am. Waiting for same reply", []),
+  {next_state, wait, S};
+wait(not_yet, S = #state{}) ->
+  notice(S, "Other not ready yet", []),
+  {next_state, wait, S};
+wait('ready!', S=#state{}) ->
+  am_ready(S#state.other),
+  ack_trans(S#state.other),
+  gen_fsm:reply(S#state.from, ok),
+  notice(S, "other side is ready. Moving to ready state", []),
+  {next_state, ready, S};
+%% Don't care about these!
+wait(Event, Data) ->
+  unexpected(Event, wait),
+  {next_state, wait, Data}.
+
+%% ready
+ready(ack, S=#state{}) ->
+  case priority(self(), S#state.other) of
+    true ->
+      try
+        notice(S, "asking for commit", []),
+        ready_commit = ask_commit(S#state.other),
+        notice(S, "ordering commit", []),
+        ok = do_commit(S#state.other),
+        notice(S, "committing...", []),
+        commit(S),
+        {stop, normal, S}
+      catch Class:Reason ->
+        %% abort! Either ready_commit or do_commit failed
+        notice(S, "commit failed", []),
+        {stop, {Class, Reason}, S}
+        end;
+    false ->
+      {next_state, ready, S}
+  end;
+ready(Event, Data) ->
+  unexpected(Event, ready),
+  {next_state, ready, Data}.
+
+ready(ask_commit, _From, S) ->
+  notice(S, "replying to ask_commit", []),
+  {reply, ready_commit, ready, S};
+ready(do_commit, _From, S) ->
+  notice(S, "committing...", []),
+  commit(S),
+  {stop, normal, ok, S};
+ready(Event, _From, Data) ->
+  unexpected(Event, ready),
+  {next_state, ready, Data}.
+
+%% commit
+commit(S = #state{}) ->
+  io:format("Transaction completed for ~s. "
+  "Items sent are:~n~p,~n received are:~n~p.~n"
+  "This operation should have some atomic save "
+  "in a database.~n",
+  [S#state.name, S#state.ownitems, S#state.otheritems]).
+
+%% The other player has sent this cancel event
+%% stop whatever we're doing and shut down!
+handle_event(cancel, _StateName, S=#state{}) ->
+  notice(S, "received cancel event", []),
+  {stop, other_cancelled, S};
+handle_event(Event, StateName, Data) ->
+  unexpected(Event, StateName),
+  {next_state, StateName, Data}.
+
+%% This cancel event comes from the client. We must warn the other
+%% player that we have a quitter!
+handle_sync_event(cancel, _From, _StateName, S = #state{}) ->
+  notify_cancel(S#state.other),
+  notice(S, "cancelling trade, sending cancel event", []),
+  {stop, cancelled, ok, S};
+%% Note: DO NOT reply to unexpected calls. Let the call-maker crash!
+handle_sync_event(Event, _From, StateName, Data) ->
+  unexpected(Event, StateName),
+  {next_state, StateName, Data}.
+
+handle_info({'DOWN', Ref, process, Pid, Reason}, _, S=#state{other=Pid, monitor=Ref}) ->
+  notice(S, "Other side dead", []),
+  {stop, {other_down, Reason}, S};
+handle_info(Info, StateName, Data) ->
+  unexpected(Info, StateName),
+  {next_state, StateName, Data}.
+
+code_change(_OldVsn, StateName, Data, _Extra) ->
+  {ok, StateName, Data}.
+ 
+%% Transaction completed.
+terminate(normal, ready, S=#state{}) ->
+  notice(S, "FSM leaving.", []);
+terminate(_Reason, _StateName, _StateData) ->
+  ok.
+
 %% add an item to an item list
 add(Item, Items) ->
   [Item | Items].
@@ -80,7 +230,7 @@ trade(OwnPid, OtherPid) ->
 
 %% accept someone's trade offer
 accept_trade(OwnPid) ->
-  gen_fsm:sync_send_event(OwnPid, accept_negotitate).
+  gen_fsm:sync_send_event(OwnPid, accept_negotiate).
 
 %% send an item on the table to be traded
 make_offer(OwnPid, Item) ->
@@ -142,3 +292,6 @@ do_commit(OtherPid) ->
 
 notify_cancel(OtherPid) ->
   gen_fsm:send_all_state_event(OtherPid, cancel).
+
+priority(OwnPid, OtherPid) when OwnPid > OtherPid -> true;
+priority(OwnPid, OtherPid) when OwnPid < OtherPid -> false.
