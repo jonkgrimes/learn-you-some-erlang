bfe76a6c012b756642bc803b67e677868099f826
Jon Grimes <jonkgrimes@gmail.com>
2014-04-01 18:08:09 -0500

    Saving my place

--- curling_scoreboard_hw.erl
+++ curling_scoreboard_hw.erl
@@ -7,4 +7,10 @@ set_teams(TeamA,TeamB) ->
   io:format("Scoreboard: Team ~s vs. Team ~s~n", [TeamA,TeamB]).
 
 next_round() ->
-  io:format("Scoreboard: round over~n")
+  io:format("Scoreboard: round over~n").
+
+add_point(Team) ->
+  io:format("Scoreboard: increased score of team ~s by 1~n", [Team]).
+
+reset_board() ->
+  io:format("Scoreboard: All teams are undefined and all scores are 0~n").
