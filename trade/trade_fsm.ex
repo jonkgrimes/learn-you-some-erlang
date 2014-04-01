fe01bd77e900defcda15a4097b0536b3e6db920c
Jon Grimes <jonkgrimes@gmail.com>
2014-04-01 16:54:03 -0500

    Duplicating code here

--- trade/trade_fsm.ex
+++ trade/trade_fsm.ex
@@ -8,10 +8,38 @@ defmodule TradeFSM do
     :monitor,
     :from
 
-  # Public API
+  ## Public API
   def start(name) do
     :gen_fsm.start(__MODULE__,[name],[])
   end
 
-  # Callbacks
+  def start_link(name) do
+    :gen_fsm.start_link(__MODULE__,[name],[])
+  end
+
+  def trade(own_pid,other_pid) do
+    :gen_fsm.sync_send_event(own_pid, { :negotiate, other_pid }, 30000)
+  end
+
+  def accept_trade(own_pid) do
+    :gen_fsm.sync_send_event(own_pid, :accept_negotiate)
+  end
+
+  def make_offer(own_pid,item) do
+    :gen_fsm.send_event(own_pid, { :make_offer, item })
+  end
+
+  def retract_offer(own_pid,item) do
+    :gen_fsm.send_event(own_pid, { :retract_offer, item })
+  end
+
+  def ready(own_pid) do
+    :gen_fsm.sync_send_event(own_pid,:ready,:infinity)
+  end
+
+  def cancel(own_pid) do
+    :gen_fsm.sync_send_all_state_event(own_pid, :cancel)
+  end
+
+  ## Callbacks
 end
