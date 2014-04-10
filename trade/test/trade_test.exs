03a572bf2aaecf87a4fbe5d1845d998f490b2a98
Jon Grimes <jonkgrimes@gmail.com>
2014-04-10 17:54:51 -0500

    These are too similar but saving my place anyways

--- trade/test/trade_test.exs
+++ trade/test/trade_test.exs
@@ -1,7 +1,20 @@
 defmodule TradeTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  setup do
+    { status, pid } = Trade.Trader.start_link("Bob")
+    { status, pid: pid }
+  end
+
+  test "idle to idle_waiting transition",meta do
+    msg = { :ask_negotiate, self() }
+    transition = :gen_fsm.send_event(meta[:pid],msg)
+    assert transition == :ok
+  end
+
+  test "idle needs to get ask_negotiate first",meta do
+    msg = { :negotiate, self() }
+    transition = :gen_fsm.send_event(meta[:pid],msg)
+    assert transition == :ok
   end
 end
