736e8f22a506fd6ab063451ba8163b23f5ddff6a
Jon Grimes <jonkgrimes@gmail.com>
2014-04-12 16:10:05 -0500

    Apparently a pid can't be written to a string

--- trade/lib/trade/trader.ex
+++ trade/lib/trade/trader.ex
@@ -18,21 +18,53 @@ defmodule Trade.Trader do
     { :ok, :idle, State.new }
   end
 
-  # Idle state
-
+  # Idle state before any trade is done
+  # We ask the other player for a negotiation and wait
+  # for our own user to accept the trade. We store the other Pid
+  # for future uses
   def idle({ :ask_negotiate, other_pid },state) do
     ref = Process.monitor(other_pid)
     notice(state,"asked for a trade negotiation.",[other_pid])
     { :next_state, :idle_wait, State.new(other: other_pid,monitor: ref) }
   end
 
-  def negotitate do
+  def idle(event,data) do
+    unexpected(:idle,event)
+    { :next_state, :idle, data }
+  end
 
+  def idle({ :negotiate, other_pid },from,state) do
+    ask_negotiate(other_pid,self())
+    notice(state,"asking user #{other_pid} for a trade",[other_pid])
+    ref = Process.monitor(other_pid)
+    { :next_state, :idle_wait, State.new(other: other_pid,monitor: ref) }
+  end
+
+  def idle(event,_from,data) do
+    unexpected(:idle,event)
+    { :next_state, :idle, data }
+  end 
+
+  # Negotiate state
+
+  def negotitate do
+     
+  end
+  
+  # Client-to-client API
+
+  def ask_negotiate(other_pid,own_pid) do
+    :gen_fsm.send_event(other_pid, { :ask_negotiate, own_pid })
   end
 
   # Private functions
 
   defp notice(state,str,args) do
-    IO.puts "Message received"
+    name = [state.name|args]
+    IO.puts "#{name}: #{str}"
+  end
+
+  defp unexpected(state,msg) do
+    IO.puts "#{self()} received unknown event #{msg} while in state #{state}"
   end
 end
