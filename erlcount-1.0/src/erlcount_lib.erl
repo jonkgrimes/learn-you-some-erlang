9963f5b38f85dee070fdbb1fc299b288ceda61a2
Jon Grimes <jong@spiceworks.com>
2014-04-28 13:26:27 -0500

    Added erlcount app

---
+++ erlcount-1.0/src/erlcount_lib.erl
@@ -0,0 +1,48 @@
+-module(erlcount_lib).
+-export([find_erl/1]).
+-include_lib("kernel/include/file.hrl").
+
+find_erl(Directory) ->
+  find_erl(Directory, queue:new()).
+
+%% private
+%% dispatches based on file type
+find_erl(Name, Queue) ->
+  {ok, F = #file_info{}} = file:read_file_info(Name),
+  case F#file_info.type of
+    directory -> handle_directory(Name, Queue);
+    regular -> handle_regular_file(Name, Queue);
+    _Other -> dequeue_and_run(Queue)
+  end.
+
+handle_directory(Name, Queue) ->
+  case file:list_dir(Dir) of
+    {ok, []} ->
+      dequeue_and_run(Queue);
+    {ok, Files} ->
+      dequeue_and_run(enqueue_many(Dir, Files, Queue))
+  end.
+
+handle_regular_file(Name, Queue) ->
+  case filename:extension(Name) of
+    ".erl" ->
+      {continue, Name, fun() -> dequeue_and_run(Queue) end};
+    _NonErl ->
+      dequeue_and_run(Queue)
+  end.
+
+dequeue_and_run(Queue) ->
+  case queue:out(Queue) of
+    {empty, _ } -> done;
+    {{value, File}, NewQueue} -> find_erl(File, NewQueue)
+  end.
+
+enqueue_many(Path, Files, Queue) ->
+  F = fun(File,Q) -> queue:in(filename:join(Path,File), Q) end,
+  lists:foldl(F, Queue, Files).
+
+regex_count(Re, Str) ->
+  case re:run(Str, Re, [global]) of
+    nomatch -> 0;
+    {match, List} -> length(List)
+  end.
